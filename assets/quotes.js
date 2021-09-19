4e26904ec295ea3b742234e3529f5cf2e4501f4d
RizzTanaka <63114205+RizzGzlz@users.noreply.github.com>
2021-09-19 22:18:09 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "shirotzy_", quotes: "Whatever happens, keep breathing" },
+    { author: "shirotzy_", quotes: "just because i don't text you or ignore you, it doesn't mean i dont like you" },
+    { author: "shirotzy_", quotes: "u know what's on menu? me 'n' u  <3" },
+    { author: "shirotzy_", quotes: "are u a secret? because i can't share you with anyome else" },
+    { author: "shirotzy_", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "shirotzy_", quotes: "people say happines start with H,but it turns out mine starts with U" },
+    { author: "shirotzy_", quotes: "If one day i die, don't cry. i'll come back to bring you with me <3" },
 ];
